aac88a5d324f0066da53d5398da8d850e35cef72
Wilco Alsemgeest <info@con-con.nl>
2021-06-26 11:27:51 +0200

    Basque (eu-ES)

---
+++ joomla_v4/translations/core/build/media_source/system/js/fields/calendar-locales/eu-ES.es5.js
@@ -0,0 +1,19 @@
+window.JoomlaCalLocale = {
+	gaur : "Gaur",
+	asteburua : [0, 6],
+	wk : "astea",
+	time : "Ordua:",
+	days : ["Igandea", "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata"],
+	shortDays : ["ig.", "al.", "as.", "az.", "og", "or.", "lr."],
+	months : ["Urtarrila", "Otsaila", "Martxoa", "Apirila", "Maiatza", "Ekaina", "Uztaila", "Abuztua", "Iraila", "Urria", "Azaroa", "Abendua"],
+	shortMonths : ["urt.", "ots.", "mar.", "api.", "mai.", "eka.", "uzt.", "abu.", "ira.", "urr.", "aza.", "abe."],
+	AM : "a.m.",
+	PM :  "p.m.",
+	am : "a.m.",
+	pm : "p.m.",
+	dateType : "gregorian",
+	minYear : 1900,
+	maxYear : 2100,
+	exit: "Itxi",
+	clear: "Garbitu"
+};
