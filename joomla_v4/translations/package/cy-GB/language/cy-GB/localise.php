a641b39cd81fb022a5ecd7d94216267dc59f4fa6
Wilco Alsemgeest <info@con-con.nl>
2021-06-26 11:11:28 +0200

    Welsh (cy-GB)

---
+++ joomla_v4/translations/package/cy-GB/language/cy-GB/localise.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package    Joomla.Language
+ *
+ * @copyright  (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * cy-GB localise class.
+ *
+ * @since  1.6
+ */
+abstract class Cy_GBLocalise
+{
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   integer  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			return array('ZERO', '0');
+		}
+		elseif ($count == 1)
+		{
+			return array('ONE', '1');
+		}
+	elseif ($count == 2)
+	{
+		return array('TWO', '2');
+	}
+	elseif ($count == 3)
+	{
+		return array('FEW', '3');
+	}
+	elseif ($count == 6)
+	{
+		return array('MANY', '6');
+	}
+		else
+		{
+			return array('OTHER', 'MORE');
+		}
+	}
+}
