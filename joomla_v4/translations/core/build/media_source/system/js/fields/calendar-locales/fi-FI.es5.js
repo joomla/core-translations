be19e6f1d3e55cae490c0775ef601593d51344ea
Wilco Alsemgeest <info@con-con.nl>
2021-06-27 10:15:33 +0200

    fi-FI

---
+++ joomla_v4/translations/core/build/media_source/system/js/fields/calendar-locales/fi-FI.es5.js
@@ -0,0 +1,19 @@
+window.JoomlaCalLocale = {
+	today : "Tänään",
+	weekend : [0, 6],
+	wk : "vko",
+	time : "Aika:",
+	days : ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"],
+	shortDays : ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"],
+	months : ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"],
+	shortMonths : ["Tam", "Hel", "Maa", "Huh", "Tou", "Kes", "Hei", "Elo", "Syy", "Lok", "Mar", "Jou"],
+	AM : "AM",
+	PM :  "PM",
+	am : "am",
+	pm : "pm",
+	dateType : "gregorian",
+	minYear : 1900,
+	maxYear : 2100,
+	exit: "Sulje",
+	clear: "Tyhjennä"
+};
