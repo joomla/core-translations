28f098504592993e9729767c1fc7eb6eeb217c82
Translation <release+translation-bot@joomla.org>
2022-08-15 08:37:25 +0000

    J4 Japanese Language update on 2022-08-15

--- joomla_v4/translations/package/ja-JP/administrator/language/ja-JP/localise.php
+++ joomla_v4/translations/package/ja-JP/administrator/language/ja-JP/localise.php
@@ -1,12 +1,17 @@
 <?php
+
 /**
  * @package    Joomla.Language
  *
  * @copyright  (C) 2011 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
+
+ * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
 
-defined('_JEXEC') or die;
+
 
 /**
  * ja-jP localise class.
@@ -24,21 +29,16 @@ abstract class Ja_JPLocalise
 	 *
 	 * @since   1.6
 	 */
-	public static function getPluralSuffixes($count)
-	{
-		if ($count == 0)
-		{
-			return array('0');
-		}
-		elseif ($count == 1)
-		{
-			return array('ONE', '1');
-		}
-		else
-		{
-			return array('OTHER', 'MORE');
-		}
-	}
+    public static function getPluralSuffixes($count)
+    {
+        if ($count == 0) {
+            return array('0');
+        } elseif ($count == 1) {
+            return array('ONE', '1');
+        } else {
+            return array('OTHER', 'MORE');
+        }
+    }
 
 	/**
 	 * Returns the ignored search words
--- joomla_v4/translations/package/ja-JP/language/ja-JP/localise.php
+++ joomla_v4/translations/package/ja-JP/language/ja-JP/localise.php
@@ -1,12 +1,17 @@
 <?php
+
 /**
  * @package    Joomla.Language
  *
  * @copyright  (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
+
+ * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
 
-defined('_JEXEC') or die;
+
 
 /**
  * ja-JP localise class.
@@ -24,19 +29,14 @@ abstract class Ja_JPLocalise
 	 *
 	 * @since   1.6
 	 */
-	public static function getPluralSuffixes($count)
-	{
-		if ($count == 0)
-		{
-			return array('0');
-		}
-		elseif ($count == 1)
-		{
-			return array('ONE', '1');
-		}
-		else
-		{
-			return array('OTHER', 'MORE');
-		}
-	}
+    public static function getPluralSuffixes($count)
+    {
+        if ($count == 0) {
+            return array('0');
+        } elseif ($count == 1) {
+            return array('ONE', '1');
+        } else {
+            return array('OTHER', 'MORE');
+        }
+    }
 }
